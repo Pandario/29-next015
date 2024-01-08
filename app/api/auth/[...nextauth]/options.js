83193095e397c0167d528cdccc8d52aff3ad45a2
Andrew <dinogalaktika@gmail.com>
2024-01-08 18:09:10 +0100

    first start

---
+++ app/api/auth/[...nextauth]/options.js
@@ -0,0 +1,104 @@
+import GitHubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
+import CredentialsProvider from "next-auth/providers/credentials";
+import User from '@/app/(models)/User';
+import bcrypt from "bcrypt";
+
+export const options = {
+    providers: [
+        GitHubProvider({
+            profile(profile) {
+                console.log('Profile Github', profile);
+                let userRole = "Github user";
+
+                if(profile?.email == "anlindeob@gmail.com") {
+                    userRole = "admin";
+                }
+
+                return {
+                    ...profile,
+                    role: userRole,
+                };
+            },
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET,
+        }),
+
+        GoogleProvider({
+            profile(profile) {
+                console.log('Profile Google', profile);
+
+                let userRole = "Google user";
+
+                return {
+                    ...profile,
+                    id: profile.sub,
+                    role: userRole,
+                };
+            },
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_SECRET,
+        }),
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                email: {
+                    label: "email",
+                    type: "text",
+                    placeholder: "your email",
+                },
+                password: {
+                    label: "password",
+                    type: "password",
+                    placeholder: "your-password",
+                },
+            },
+            async authorize(credentials) {
+                try {
+                    const foundUser = User.findOne({email: credentials.email}).lean().exec();
+
+                    if(foundUser) {
+                        console.log("User exists")
+                        const match = bcrypt.compare(
+                            credentials.password, 
+                            foundUser.password
+                            );
+                        if(match) {
+                            console.log("Good");
+                            delete foundUser.password;
+                            let userRole = "unatorised";
+                                if(credentials?.email == "bob@bob.com") {
+                                    userRole = "admin";
+                                }
+                            return {...foundUser, email: credentials.email,  role: userRole,}
+                        }
+                    }
+                } catch (error) {
+                    console.log(error);
+                }
+                return null;
+            }
+        }),
+    ],
+    callbacks: {
+        async jwt({token, user}) {
+            if(user) token.role = user.role
+            return token
+        },
+        async session({session, token}) {
+            if(session?.user) session.user.role = token.role
+            return session;
+        },
+    },
+};
+
+/**callbacks: {
+        async jwt({token, user}) {
+            if(user) token.role = user.role
+            return token
+        },
+        async session({session, token}) {
+            if(session?.user) session.user.role = token.role
+            return session;
+        },
+    }, */
--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+import Nav from './(components)/Nav'
+import AuthProvider from './(components)/AuthProvider'
+import './globals.css'
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,7 +11,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <AuthProvider>
+        <body className='bg-gray-100'>
+          <Nav/>
+          
+          <div className='m-2'>
+            {children}
+          </div>
+          
+        </body>
+      </AuthProvider>
     </html>
   )
 }
